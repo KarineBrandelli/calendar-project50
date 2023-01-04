19448361d0427d4e0a86fe1d10b128f6d4829a39
Karine Brandelli <karinebrandelli@live.com>
2023-01-04 11:42:15 -0300

    build: import Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Home from './pages/Home'
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to first version of Calendar Project50.</h1>
+      <Home />
     </div>
   )
 }
