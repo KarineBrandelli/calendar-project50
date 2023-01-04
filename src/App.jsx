87a9a77bba007d855879980765665d4387707364
Karine Brandelli <karinebrandelli@live.com>
2023-01-04 13:08:25 -0300

    build: separate Button component from Header and Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import Home from './pages/Home'
+import Button from "./components/Button";
+import Header from "./components/Header";
+import Home from './pages/Home';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Button />
+      <div className="main-content">
+        <Header />
+        <Home />
+      </div>
     </div>
   )
 }
