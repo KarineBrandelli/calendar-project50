9a03b7a32cc1f2166a9bfd6deae4bc5830460b76
wesleydamasceno <wesleydamasceno.co@gmail.com>
2023-01-04 19:56:58 -0300

    build: add component Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import githubIcon from '../../assets/github-icon.svg';
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <a href="#">
+        <img src={githubIcon} />
+      </a>
+    </footer>
+  )
+}
