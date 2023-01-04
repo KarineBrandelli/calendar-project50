ebc93e33f45291c544e9a7fcbd952522f48b8391
Karine Brandelli <karinebrandelli@live.com>
2023-01-04 13:18:31 -0300

    build: create Button and Header component, add style to Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>PROJECT50 <span>JAN 4 2023</span></h1>
+      <h2>A CHALLENGE CREATED BY STATE OF MIND.</h2>
+    </div>
+
+  )
+}
+
+export default Header
