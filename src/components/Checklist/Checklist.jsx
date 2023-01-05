5639564f7af09355830f320bf037f0b1846cb8b3
wesleydamasceno <wesleydamasceno.co@gmail.com>
2023-01-05 15:17:23 -0300

    build: add checkbox-component and update css

--- src/components/Checklist/Checklist.jsx
+++ src/components/Checklist/Checklist.jsx
@@ -1,15 +1,39 @@
+import { CheckboxInput } from '../Checkbox/Checkbox';
 import "./Checklist.css";
 
 export const Checklist = () => {
   return (
-    <ul className="checklist-items">
-      <li>WAKE UP BEFORE 8 A.M</li>
-      <li>READ 10 PAGES</li>
-      <li>1H MORNING ROUTINE</li>
-      <li>FOLLOWED A HELTHY DIET</li>
-      <li>1H EXERCISES</li>
-      <li>TRACK YOUR PROGRESS</li>
-      <li>1H LEARNING A NEW SKILL</li>
-    </ul>
+    <>
+      <ul className="checklist-items">
+        <li>
+          <CheckboxInput />
+          WAKE UP BEFORE 8 A.M
+        </li>
+        <li>
+          <CheckboxInput />
+          READ 10 PAGES
+        </li>
+        <li>
+          <CheckboxInput />
+          1H MORNING ROUTINE
+        </li>
+        <li>
+          <CheckboxInput />
+          FOLLOWED A HELTHY DIET
+        </li>
+        <li>
+         <CheckboxInput />
+          1H EXERCISES
+        </li>
+        <li>
+          <CheckboxInput />
+          TRACK YOUR PROGRESS
+        </li>
+        <li>
+          <CheckboxInput />
+          1H LEARNING A NEW SKILL
+        </li>
+      </ul>
+    </>
   )
 }
