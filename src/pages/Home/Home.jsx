b1cc80ff89f1d03fc6cc6ac527e701f02ded6dfe
Karine Brandelli <karinebrandelli@live.com>
2023-01-05 13:57:07 -0300

    build: import Day component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,17 @@
 import { Header } from '../../components/Header/Header';
 import { Calendar } from '../../components/Calendar/Calendar';
 import { Footer } from '../../components/Footer/Footer';
+import { Day } from '../../components/Day/Day';
 
 import './Home.css';
 
 export const Home = () => {
   return (
     <div className="home-container">
-      <Header />
+      {/* <Header />
       <Calendar />
-      <Footer />
+      <Footer /> */}
+      <Day />
     </div>
-  );
-};
+  )
+}
