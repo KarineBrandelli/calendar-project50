9e5c2319a34ef1450059b7480bc73c4816c38e29
wesleydamasceno <wesleydamasceno.co@gmail.com>
2023-01-05 15:15:17 -0300

    style: update background and size

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ import './Button.css';
 export const Button = () => {
   return (
     <button onClick={() => window.close()} className="close_button">
-      <X size={28} color="#8E2E2E"/>
+      <X size={32} color="#8E2E2E"/>
     </button>
   )
 }
