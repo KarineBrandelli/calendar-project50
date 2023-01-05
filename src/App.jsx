13022cd1cdc6418e8f17cc09b52ad692fb0d54c6
wesleydamasceno <wesleydamasceno.co@gmail.com>
2023-01-05 16:35:12 -0300

    feat: add react-router-dom config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import { Home } from './pages/Home/Home';
 import { Button } from "./components/Button/Button";
+import { Day } from './pages/Day/Day';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/day/:id",
+    element: <Day />,
+  }
+]);
 
 export const App = () => {
   return (
     <>
       <Button />
-      <Home />
+      <RouterProvider router={router} />
     </>
   )
 }
