1e2975de6a2c95c63802ca21ce8047fa5895e8b0
Karine Brandelli <karinebrandelli@live.com>
2023-01-05 13:55:31 -0300

    build: create Checklist component and add style

---
+++ src/components/Checklist/Checklist.jsx
@@ -0,0 +1,15 @@
+import "./Checklist.css";
+
+export const Checklist = () => {
+  return (
+    <ul className="checklist-items">
+      <li>WAKE UP BEFORE 8 A.M</li>
+      <li>READ 10 PAGES</li>
+      <li>1H MORNING ROUTINE</li>
+      <li>FOLLOWED A HELTHY DIET</li>
+      <li>1H EXERCISES</li>
+      <li>TRACK YOUR PROGRESS</li>
+      <li>1H LEARNING A NEW SKILL</li>
+    </ul>
+  )
+}
