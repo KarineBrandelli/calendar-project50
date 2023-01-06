aacc6b503aacd5dafb33096aad607da4677c0a2f
wesleydmscn <wesleydamasceno.co@gmail.com>
2023-01-06 18:28:43 -0300

    feat: add api-request to json-server

--- src/components/Checklist/Checklist.jsx
+++ src/components/Checklist/Checklist.jsx
@@ -1,19 +1,34 @@
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import { useState } from "react";
+
 import { CheckboxInput } from "../Checkbox/Checkbox";
 import { listItems } from "./ChecklistItems";
+
 import "./Checklist.css";
 
 export const Checklist = () => {
+  const { id } = useParams();
   const [checkedState, setCheckedState] = useState(
     new Array(listItems.length).fill(false)
   );
 
+  function getData() {
+    axios({
+      method: "get",
+      url: `http://localhost:3000/day_${id}`,
+    })
+    .then((res) => {
+      console.log(res.data.tasks);
+    });
+  }
+
   const handleOnChange = (position) => {
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-      );
-      console.log(updatedCheckedState);
-      
+    const updatedCheckedState = 
+      checkedState.map((item, index) => index === position ? !item : item);
+    
+    console.log(updatedCheckedState);
+  
     setCheckedState(updatedCheckedState);
   };
 
@@ -29,7 +44,7 @@ export const Checklist = () => {
                 value={name}
                 marked={checkedState[index]}
                 changedMark={() => handleOnChange(index)}
-              /> 
+              />
               <label>{name}</label>
             </li>
           );
