b55dfe16dbd4909ad0746bcf5a23036affa5397c
Karine Brandelli <karinebrandelli@live.com>
2023-01-06 16:10:16 -0300

    build: set props to CheckboxInput function

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,11 +1,13 @@
 import Checkbox from '@mui/material/Checkbox';
 import { Square, CheckSquare } from 'phosphor-react';
 
-export const CheckboxInput = () => {
+export const CheckboxInput = (props) => {
   return (
     <Checkbox
       icon={<Square size={32} color="white"/>}
       checkedIcon={<CheckSquare size={32} color="white"/>}
+      checked={props.marked}
+      onChange={props.changedMark}
     />
   )
-} 
+}
