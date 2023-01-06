5db963204942ad47f0a6650cab36f411faa8d354
Karine Brandelli <karinebrandelli@live.com>
2023-01-06 16:09:29 -0300

    build: replace old list for dinamic array

--- src/components/Checklist/Checklist.jsx
+++ src/components/Checklist/Checklist.jsx
@@ -1,39 +1,40 @@
-import { CheckboxInput } from '../Checkbox/Checkbox';
+import { useState } from "react";
+import { CheckboxInput } from "../Checkbox/Checkbox";
+import { listItems } from "./ChecklistItems";
 import "./Checklist.css";
 
 export const Checklist = () => {
+  const [checkedState, setCheckedState] = useState(
+    new Array(listItems.length).fill(false)
+  );
+
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+      );
+      console.log(updatedCheckedState);
+      
+    setCheckedState(updatedCheckedState);
+  };
+
   return (
     <>
       <ul className="checklist-items">
-        <li>
-          <CheckboxInput />
-          WAKE UP BEFORE 8 A.M
-        </li>
-        <li>
-          <CheckboxInput />
-          READ 10 PAGES
-        </li>
-        <li>
-          <CheckboxInput />
-          1H MORNING ROUTINE
-        </li>
-        <li>
-          <CheckboxInput />
-          FOLLOWED A HELTHY DIET
-        </li>
-        <li>
-         <CheckboxInput />
-          1H EXERCISES
-        </li>
-        <li>
-          <CheckboxInput />
-          TRACK YOUR PROGRESS
-        </li>
-        <li>
-          <CheckboxInput />
-          1H LEARNING A NEW SKILL
-        </li>
+        {listItems.map(({ name }, index) => {
+          return (
+            <li key={index}>
+              <CheckboxInput
+                id={index}
+                name={name}
+                value={name}
+                marked={checkedState[index]}
+                changedMark={() => handleOnChange(index)}
+              /> 
+              <label>{name}</label>
+            </li>
+          );
+        })}
       </ul>
     </>
-  )
-}
+  );
+};
