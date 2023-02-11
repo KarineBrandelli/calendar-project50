825f89177fc1f6e837d01ecf647e43d85ee6944e
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-02-11 18:20:37 -0300

    build: add electron-defaults --> main | preload | renderer

---
+++ src/renderer/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { GithubLogo } from 'phosphor-react'
+import './Footer.css'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <a href="#">
+        <GithubLogo size={32} color="white" />
+      </a>
+    </footer>
+  )
+}
---
+++ src/renderer/src/pages/Day/Day.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+import { Header } from '../../components/Header/Header'
+import { Checklist } from '../../components/Checklist/Checklist'
+import { ArrowLeft } from 'phosphor-react'
+
+import './Day.css'
+
+export const Day = () => {
+  return (
+    <div className="day-container grid">
+      <Link to={`/`}>
+        <ArrowLeft className="arrow-left" size={24} color="white" />
+      </Link>
+
+      <Header />
+      <Checklist />
+    </div>
+  )
+}
