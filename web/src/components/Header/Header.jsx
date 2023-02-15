59bb1e35f6b7fca7d750a87a62093f5fbd68a7ee
Karine Brandelli <karinebrandelli@live.com>
2023-02-15 15:38:45 -0300

    build: add components to web version

---
+++ web/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import { useParams } from 'react-router-dom'
+import './Header.css'
+
+export const Header = () => {
+  const { id } = useParams()
+  const getTodayDate = new Date().toDateString()
+  const getPageParams = `Day ${id}`
+
+  return (
+    <header className="header grid">
+      <h1 className="title flex">
+        PROJECT50 <span className="current-date">{id ? getPageParams : getTodayDate}</span>
+      </h1>
+      <h2 className="subtitle">A CHALLENGE CREATED BY STATE OF MIND.</h2>
+    </header>
+  )
+}
