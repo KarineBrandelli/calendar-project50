c9b22e43a8062ae3cc117a1f6e2b98b4909eb82d
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-02-18 09:43:34 -0300

    build(web): replace RouterProvider with BrowserRouter

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,21 +1,16 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Home } from './pages/Home/Home'
 import { Day } from './pages/Day/Day'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/day/:id',
-    element: <Day />
-  }
-])
-
 export const App = () => {
   return (
-    <RouterProvider router={router} />
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/day/:id" element={<Day />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
